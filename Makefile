Be-Core-Code/curso-intro-jenkins-modulo-5-sample-repository-with-c-code

[cmocka] Extrae modulo stack.h

Makefile
@@ -1,11 +1,13 @@
-is_amstrong_number : main.o 
-	gcc main.o -o is_armstrong_number -lm 
+is_amstrong_number : main.o stack.o 
+	gcc main.o stack.o -o is_armstrong_number -lm 
 main.o : main.c
 	gcc -c main.c
+stack.o : stack.c
+	gcc -c stack.c	
 clean-reports:
 	rm -f reports/cppcheck/*
 clean: clean-reports
-	rm main.o is_armstrong_number
+	rm -f main.o stack.o is_armstrong_number
 
 cppcheck-xml :
 	cppcheck *.c --xml --xml-version=2 --enable=all --inconclusive --language=c *.c 2>reports/cppcheck/report.xml

alfonsoalba-cursos/armstrong-number-c

Makefile
@@ -1,11 +1,13 @@
-is_armstrong_number : main.o 
-	gcc main.o -o is_armstrong_number -lm 
+is_armstrong_number : main.o stack.o 
+	gcc main.o stack.o -o is_armstrong_number -lm 
 main.o : main.c
 	gcc -c main.c
+stack.o : stack.c
+	gcc -c stack.c	
 clean-reports:
 	rm -f reports/cppcheck/*
 clean: clean-reports
-	rm main.o is_armstrong_number
+	rm -f main.o stack.o is_armstrong_number
 
 cppcheck-xml :
 	cppcheck *.c --xml --xml-version=2 --enable=all --inconclusive --language=c *.c 2>reports/cppcheck/report.xml