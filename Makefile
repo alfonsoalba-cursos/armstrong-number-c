Be-Core-Code/curso-intro-jenkins-modulo-5-sample-repository-with-c-code

[cmocka] Añade tests para la función is_armstrong_number

Makefile
@@ -1,4 +1,4 @@
-is_amstrong_number : main.o stack.o armstrong.o
+is_amstrong_number : objects
 	gcc main.o stack.o armstrong.o -o is_armstrong_number -lm 
 main.o : main.c
 	gcc -c main.c
@@ -6,10 +6,17 @@ armstrong.o : armstrong.c
 	gcc -c armstrong.c
 stack.o : stack.c
 	gcc -c stack.c	
+objects : main.o stack.o armstrong.o
+
+clean-objects: 
+	rm -f main.o stack.o armstrong.o
 clean-reports:
 	rm -f reports/cppcheck/*
-clean: clean-reports
-	rm -f main.o stack.o is_armstrong_number
+
+clean-tests:
+	rm -rf tests/build/*
+clean: clean-reports clean-objects clean-tests
+	rm -f is_armstrong_number
 
 cppcheck-xml :
 	cppcheck *.c --xml --xml-version=2 --enable=all --inconclusive --language=c *.c 2>reports/cppcheck/report.xml
@@ -17,3 +24,7 @@ cppcheck :
 	cppcheck *.c --enable=all --inconclusive --language=c *.c
 doc:
 	doxygen
+
+tests: clean armstrong.o stack.o
+	gcc tests/test_is_armstrong_number.c armstrong.o stack.o -lm -lcmocka -o tests/build/test_is_armstrong_number
+	./tests/build/test_is_armstrong_number 

alfonsoalba-cursos/armstrong-number-c

Makefile
@@ -1,4 +1,4 @@
-is_armstrong_number : main.o stack.o armstrong.o
+is_armstrong_number : objects
 	gcc main.o stack.o armstrong.o -o is_armstrong_number -lm 
 main.o : main.c
 	gcc -c main.c
@@ -6,10 +6,17 @@ armstrong.o : armstrong.c
 	gcc -c armstrong.c
 stack.o : stack.c
 	gcc -c stack.c	
+objects : main.o stack.o armstrong.o
+
+clean-objects: 
+	rm -f main.o stack.o armstrong.o
 clean-reports:
 	rm -f reports/cppcheck/*
-clean: clean-reports
-	rm -f main.o stack.o is_armstrong_number
+
+clean-tests:
+	rm -rf tests/build/*
+clean: clean-reports clean-objects clean-tests
+	rm -f is_armstrong_number
 
 cppcheck-xml :
 	cppcheck *.c --xml --xml-version=2 --enable=all --inconclusive --language=c *.c 2>reports/cppcheck/report.xml
@@ -17,3 +24,7 @@ cppcheck :
 	cppcheck *.c --enable=all --inconclusive --language=c *.c
 doc:
 	doxygen
+
+tests: clean armstrong.o stack.o
+	gcc tests/test_is_armstrong_number.c armstrong.o stack.o -lm -lcmocka -o tests/build/test_is_armstrong_number
+	./tests/build/test_is_armstrong_number 